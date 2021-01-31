16d6493a313fae0dd6785d116035053556259e71
alkhioz <alemendoza0197@gmail.com>
2021-01-30 19:16:23 -0500

    added images to instructions

--- scenes/oldManRoom/Instructions.gd
+++ scenes/oldManRoom/Instructions.gd
@@ -12,15 +12,18 @@ func _ready():
 	var seconds = 3
 	match global.instruction_controler:
 		0:
-			get_node("intructionText").text = "Press one of the buttons"
+#			get_node("intructionText").text = "Press one of the buttons"
+			get_node("image").texture = load("res://assets/images/oldManRoom/transitions/transitionChild.png")
 			yield(get_tree().create_timer(seconds), "timeout")
 			get_tree().change_scene("res://scenes/childMiniGame/ChildMiniGame.tscn")
 		1:
-			get_node("intructionText").text  = "Use A,S,D,F to play the notes"
+#			get_node("intructionText").text  = "Use A,S,D,F to play the notes"
+			get_node("image").texture = load("res://assets/images/oldManRoom/transitions/transitionYouth.png")
 			yield(get_tree().create_timer(seconds), "timeout")
 			get_tree().change_scene("res://scenes/youthMiniGame/youthMiniGame.tscn")
 		2:
-			get_node("intructionText").text = "Play A to jump and evade obstacles"
+#			get_node("intructionText").text = "Play A to jump and evade obstacles"
+			get_node("image").texture = load("res://assets/images/oldManRoom/transitions/transitionAdult.png")
 			yield(get_tree().create_timer(seconds), "timeout")
 			get_tree().change_scene("res://scenes/adultMiniGame/adultMiniGame.tscn")
 
